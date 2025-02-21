c4a8da131f3ec62c6f167c4bf69eb3d2f5b9ad4c
Neelaveni88 <r.neelaveni2021@gmail.com>
2025-02-21 19:09:09 +0530

    react - router

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,40 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import WithAuthonication from './Components/WithAuthonication'
-import Profile from './Components/Profile'
-// import SimpleCode from './Components/SimpleCode'
-import AnniversaryCard from './Components/AnniversaryCard'
-import UseRefForm from './Components/UseRef/UseRefForm'
+// import WithAuthonication from './Components/WithAuthonication'
+// import Profile from './Components/Profile'
+// // import SimpleCode from './Components/SimpleCode'
+// import AnniversaryCard from './Components/AnniversaryCard'
+// import UseRefForm from './Components/UseRef/UseRefForm'
 import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from './Components/NavBar'
+import Homes from "./Components/Pages/Homes"
+import About from "./Components/Pages/About"
+import Contact from "./Components/Pages/Contact"
+import NotFound from './Components/Pages/NotFound'
+import { Routes,Route } from 'react-router-dom'
 
 
 function App() {
   return (
 
     <>
-      <ToggleTheme/>
+      {/* <ToggleTheme/> */}
      
+     <NavBar/>
+     <Routes>
+      <Route path='/' element={<Homes/>}/>
+      <Route path='/About' element={<About/>}/>
+      <Route path='/Contact' element={<Contact/>}/>
+      <Route path="*" element={<NotFound />} />
+     </Routes>
      
-    <h1>Higher Order Components</h1>
+    
       {/* <SimpleCode/> */}
-     <AnniversaryCard/>
+     {/* <AnniversaryCard/>
      <WithAuthonication/>
      <Profile/>
-     <UseRefForm/>
+     <UseRefForm/> */}
 
     </>
   )
