29693d9593a14d47f706e8d0c19953a705c3b756
Neelaveni88 <r.neelaveni2021@gmail.com>
2025-02-22 12:40:25 +0530

    NavbarAbout

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,44 @@
 import React from 'react'
-import ToggleTheme from './ToggleTheme'
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import ToggleTheme from './ToggleTheme'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
-import WithAuthonication from './Components/WithAuthonication'
-import Profile from './Components/Profile'
+// import WithAuthonication from './Components/WithAuthonication'
+// import Profile from './Components/Profile'
 // import SimpleCode from './Components/SimpleCode'
-import AnniversaryCard from './Components/AnniversaryCard'
-import UseRefForm from './Components/UseRef/UseRefForm'
+// import AnniversaryCard from './Components/AnniversaryCard'
+// import UseRefForm from './Components/UseRef/UseRefForm'
 import "bootstrap/dist/css/bootstrap.min.css";
+import { FormProvider } from './Components/Revision/FormContext';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Components/Revision/Home';
+import About from './Components/Revision/About';
+import NavBar from './Components/Revision/Navbar';
 
 
 function App() {
   return (
 
     <>
-      <ToggleTheme/>
+      {/* <ToggleTheme/> */}
+     <FormProvider>
+      <NavBar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/About' element={<About/>}/>
+      </Routes>
+     </FormProvider>
+
      
-     
-    <h1>Higher Order Components</h1>
+
       {/* <SimpleCode/> */}
-     <AnniversaryCard/>
+     {/* <AnniversaryCard/>
      <WithAuthonication/>
      <Profile/>
-     <UseRefForm/>
+     <UseRefForm/> */}
+
+
 
     </>
   )
